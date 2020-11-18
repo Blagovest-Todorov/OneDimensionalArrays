aa7c5d1244a9d67cfbd6377bd8c94db2b26477e8
Blagovest-Todorov <blagovest.stod@abv.bg>
2020-11-18 09:44:16 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Numerics;
+using System.Linq;
+
+namespace _01.arrayExamplesTasks
+{
+    class Program
+    {
+        static void  Main(string[] args)
+       {
+            int[] numbers = Enumerable.Range(1, 10).ToArray();
+            
+            for (int i = 1; i < numbers.Length ; i*= 2, i++)
+            {
+                //numbers[i] = i * i;
+                int number = numbers[i];
+                
+                Console.WriteLine("index : {0}-> number :{1}",i, number);
+            }
+        }
+    }
+}
